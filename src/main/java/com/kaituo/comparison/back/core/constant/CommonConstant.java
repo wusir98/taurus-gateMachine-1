1a186aeef618a6be52e3b88c682fefb9633287a6
yedong <zhyd704@126.com>
2020-03-09 10:06:27 +0800

    commit

--- src/main/java/com/kaituo/comparison/back/core/constant/CommonConstant.java
+++ src/main/java/com/kaituo/comparison/back/core/constant/CommonConstant.java
@@ -4,11 +4,30 @@ package com.kaituo.comparison.back.core.constant;
 import com.kaituo.comparison.back.core.utils.Configuration;
 
 /**
- *
+ * 海康API
  */
 public class CommonConstant {
 
     public static final String HK_URL = "HK_URL";
-    public static final String KEY_SICKCITYINFODATA = Configuration.getInstance().getValue(CommonConstant.HK_URL) + "largeScreen/sickCityInfoData";
+    //人员管理--添加人员
+    public static final String HK_SINGLE_ADD_PEOPLE = Configuration.getInstance().getValue(CommonConstant.HK_URL) + "/resource/v1/person/single/add";
+
+    //人员管理--批量添加人员
+    public static final String HK_BATCH_ADD_PEOPLE = Configuration.getInstance().getValue(CommonConstant.HK_URL) + "/resource/v1/person/batch/add";
+
+    //人员管理--修改人员
+    public static final String HK_SINGLE_UPDATE_PEOPLE = Configuration.getInstance().getValue(CommonConstant.HK_URL) + "/resource/v1/person/single/update";
+
+    //人员管理--批量删除人员
+    public static final String HK_BATCH_DELETE_PEOPLE = Configuration.getInstance().getValue(CommonConstant.HK_URL) + "/resource/v1/person/batch/delete";
+
+    //人员管理--批量添加人脸
+    public static final String HK_SINGLE_ADD_FACE = Configuration.getInstance().getValue(CommonConstant.HK_URL) + "/resource/v1/face/single/add";
+
+    //人员管理--批量修改人脸
+    public static final String HK_SINGLE_UPDATE_FACE = Configuration.getInstance().getValue(CommonConstant.HK_URL) + "/resource/v1/face/single/update";
+
+    //人员管理--批量删除人脸
+    public static final String HK_SINGLE_DELETE_FACE = Configuration.getInstance().getValue(CommonConstant.HK_URL) + "/resource/v1/face/single/delete";
 
 }
---
+++ src/main/java/com/kaituo/comparison/back/core/service/hksdk/HkService.java
@@ -0,0 +1,21 @@
+package com.kaituo.comparison.back.core.service.hksdk;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @Description:
+ * @Author: yedong
+ * @Date: 2020/3/9 9:58
+ * @Modified by:
+ */
+@Service
+public class HkService {
+
+    @Autowired
+    RestTemplate restTemplate;
+
+
+
+}
